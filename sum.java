999d00f467e6abf8782257aea160fdabc7d5531c
R.H.Mohamed Riffath <107987180+riffathRHM@users.noreply.github.com>
2023-01-21 10:16:19 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -4,12 +4,12 @@ class sum
     {
         int a =5;
         int b =2;
-<<<<<<< HEAD
+
         int c = 6;
         int d = 7;
-=======
+
         int c =6;
->>>>>>> 047f0e744fc8168da636f891235fa596d82ce901
+
         System.out.println(a+b+c+d);
         System.out.println("sum");
         
